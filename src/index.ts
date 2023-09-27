be15b6aaf4d8b92b917f3a400f88348258359aef
Hideaki Takahashi <hi_takahashi@yumemi.co.jp>
2023-09-27 23:33:48 +0900

    feat: add connection process

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,18 @@
 import express from "express";
 import { createServer } from "http";
+import { Server } from "socket.io";
 
 const app = express();
 const httpServer = createServer(app);
 const port = 3000;
+const io = new Server(httpServer);
 
 app.get("/", (_, res) => {
   res.send("hello world");
+});
+
+io.on("connection", (socket) => {
+  console.log("connected");
 });
 
 httpServer.listen(port, () => {
