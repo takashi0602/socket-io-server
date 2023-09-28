cf6a369c848fb7b09adc0f01de53dc897a8626f9
Hideaki Takahashi <hi_takahashi@yumemi.co.jp>
2023-09-29 02:25:32 +0900

    feat: add a process using socket.io

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
 import { Room } from "./model/room";
+import { SendMessage } from "model/message";
 
 const app = express();
 const httpServer = createServer(app);
@@ -30,6 +31,23 @@ app.get("/room/:id", (req, res) => {
 
 io.on("connection", (socket) => {
   console.log("connected");
+
+  socket.on("join-room", (roomId: number) => {
+    socket.join(`${roomId}`);
+  });
+
+  socket.on("send-message", (req: SendMessage) => {
+    const index = rooms.findIndex((room) => room.id === req.roomId);
+    const id = rooms[index].messages.length + 1;
+    const message = { id, userId: req.userId, body: req.body };
+    rooms[index].messages.push(message);
+
+    io.to(`${req.roomId}`).emit("receive-message", message);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("disconnected");
+  });
 });
 
 httpServer.listen(port, () => {
